54c148a84f9b28bed65316f49e3b92653e1df97f
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-10-21 13:16:17 +0500

    some basi working releated to socket

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const {Chess} = require("chess.js");
+const path = require("path");
+const { title } = require("process");
+
+
+
+
+
 const app = express();
+const server = http.createServer(app);
 
 
-app.get("/",function(req,res){
-    res.send("it is working ")
+
+const io = socket(server);
+
+const chess = new Chess();
+// set variables
+let players ={};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req,res)=>{
+    res.render("index",{title: "Chess game"})
 })
 
-app.listen(5000);
+io.on("connection",function (uniquesocket){
+    console.log("connected");
+    
+    // for reciving write code
+    // uniquesocket.on("churan",function(){
+        // console.log("churan recieved");
+        // io.emit("churan paapdi"); // to send all reciever include sender
+        
+  
+        // disconnection 
+        // uniquesocket.on("disconnet",function(){
+        //     console.log("disconnected");
+            
+        // })
+    
+});
+
+
+
+server.listen(5000 ,()=>{
+    console.log("Listning on 5000");
+    
+});
---
+++ public/js/chessgame.js
@@ -0,0 +1,16 @@
+// basic socket io set up
+
+// it connect to fornt end to server where we write code in app.js connection part
+const socket = io();
+
+// throw something to recieve
+
+
+// socket.emit("churan");
+
+//  to recieve on front end send from backend side
+// socket.on("churan paapdi",function(){
+//     console.log("churan paadi received");
+    
+// })
+
