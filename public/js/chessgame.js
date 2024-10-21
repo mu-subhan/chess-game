0863e1ecf7e0884db2ce6b824aa890b8b9807d82
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-10-21 17:07:42 +0500

    done with frontend and backend except some functionality

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,7 +1,8 @@
 // basic socket io set up
 
+
+
 // it connect to fornt end to server where we write code in app.js connection part
-const socket = io();
 
 // throw something to recieve
 
@@ -10,7 +11,129 @@ const socket = io();
 
 //  to recieve on front end send from backend side
 // socket.on("churan paapdi",function(){
-//     console.log("churan paadi received");
+    //     console.log("churan paadi received");
     
-// })
+    // })
+    
+    
+    const socket = io();
+    const chess = new Chess();
+    const boardElement = document.querySelector(".chessboard")
 
+    // variables
+
+    let draggedPiece = null;
+    let sourceSquare = null;
+    let playerRole   = null;
+
+    // function 
+
+    const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    // console.log(board); 
+    board.forEach((row, rowindex) => {
+        row.forEach((square,squareindex) =>{
+            const squareElement = document.createElement("div");
+            squareElement.classList.add(
+                "square",
+                (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+            );
+            squareElement.dataset.row = rowindex;
+            squareElement.dataset.col = squareindex;
+
+            if(square){
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add(
+                    "piece",
+                    square.color === "w" ? "white" : "black"
+                );
+                pieceElement.innerText = getPieceUnicode(square);
+                pieceElement.draggable = playerRole === square.color;
+
+                pieceElement.addEventListener("dragstart",(e) => {
+                    if(pieceElement.draggable){
+                        draggedPiece = pieceElement;
+                        sourceSquare = {row:rowindex, col: squareindex};
+                        e.dataTransfer.setData("text/plain","");
+                    }
+                });
+
+                pieceElement.addEventListener("dragend",(e) =>{
+                    draggedPiece = null;
+                    sourceSquare = null;
+                });
+
+                squareElement.appendChild(pieceElement);
+            }
+            squareElement.addEventListener("dragover",function(e){
+                e.preventDefault();
+            });
+
+            squareElement.addEventListener("drop",function(e){
+                e.preventDefault();
+                if(draggedPiece){
+                    const targetSource = {
+                        row: parseInt(squareElement.dataset.row),
+                        col: parseInt(squareElement.dataset.col),
+                    }
+
+                    handleMove(sourceSquare,targetSource);
+                }
+            });
+            boardElement.appendChild(squareElement);
+        })
+    });
+    
+
+
+    };
+
+    const handleMove = (source,target) => {
+   const move = {
+    from: `${String.fromCharCode(97+source.col)}${8 - source.row}`,
+    to:`${String.fromCharCode(97+target.col)}${8 - target.row}`,
+    promotion:'q'
+   }  
+    };
+
+    const getPieceUnicode = (piece) => {
+        const unicodePieces = {
+            p: "♙",
+            r: "♖",
+            n: "♘",
+            b: "♗",
+            q: "♕",
+            k: "♔",
+            P: "♟",
+            R: "♜",
+            N: "♞",
+            B: "♝",
+            Q: "♛",
+            K: "♚",
+          };
+          return unicodePieces[piece.type] || "";
+    };
+
+
+    socket.on("playerRole",function (role){
+        playerRole = role;
+        renderBoard();
+    });
+
+    socket.on("spectatorRole",function(){
+        playerRole = null;
+        renderBoard();
+    });
+
+    socket.on("boardState",function (fen){
+        chess.load(fen);
+        renderBoard();
+    });
+
+    socket.on("move",function (move){
+        chess.move(move);
+        renderBoard();
+    })
+
+    renderBoard();
